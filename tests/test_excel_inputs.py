451326949d43dacdcee443021f68af47cf23e89f
Pedro <Pedro-T@users.noreply.github.com>
2024-09-22 13:20:16 -0400

    update tests for class schedule extract and add test resource file

--- tests/test_excel_inputs.py
+++ tests/test_excel_inputs.py
@@ -1,8 +1,13 @@
 import unittest
-from class_planning_tool.input_data.excel_inputs import get_cutoff_format, populate_column_semester_map
+from pathlib import Path
+from class_planning_tool.input_data.excel_inputs import get_cutoff_format, populate_column_semester_map, extract_sheet_data, get_class_schedule_data
+from openpyxl import load_workbook
 
 
 class TestClassScheduleParsing(unittest.TestCase):
+
+    def setUp(self):
+        self.resource_path: Path = Path("./tests/resources")
 
     def test_semester_cutoff_formatter_valid(self):
         self.assertEqual(241, get_cutoff_format("SP24"))
@@ -42,3 +47,27 @@ class TestClassScheduleParsing(unittest.TestCase):
         }
         self.assertDictEqual(expectation, populate_column_semester_map(row, cutoff))
     
+    def test_extract_data(self):
+        # this uses a cut down version of the course schedule in the test resources directory
+        output: dict[str, list[str]] = extract_sheet_data(load_workbook(self.resource_path / "schedule_input_test.xlsx").active)
+        self.assertListEqual(output["CPSC 1105"], ["SP25", "SU25", "FA25", "SP26", "SU26", "FA26", "SP27", "SU27", "FA27", "SP28", "SU28", "FA28", "SP29"]) # all present
+        self.assertListEqual(output["CPSC 1555"], []) # none present (blanks)
+        self.assertListEqual(output["CPSC 2105"], ["SP25", "FA25", "SP26", "FA26", "SP27", "FA27", "SP28", "FA28", "SP29"]) #  no summers, also has blanks  indicated by space or period
+        self.assertListEqual(output["CPSC 2108"], ["SP25", "SU25", "FA25", "SP26", "SU26", "FA26", "SP27", "SU27", "FA27", "SP28", "SU28", "FA28", "SP29"]) # all, some have only one letter
+        self.assertListEqual(output["CYBR 3108"], ["SP25", "SP26", "SP27", "SP28", "SP29"]) # spring only, also has ?? in one cell with a valid O
+        self.assertListEqual(output["CPSC 3137"], ["FA26", "FA28"]) # has extra text past last column, should be ignored
+        self.assertListEqual(output["CPSC 3165"], ["SP25", "SU25", "FA25", "SP26", "SU26", "FA26", "SP27", "SU27", "FA27", "SP28", "SU28", "FA28", "SP29"]) # all present, some have (May)
+        self.assertEqual(7, len(output.keys()))
+    
+    def test_extract_data_with_cutoff(self):
+        # this uses a cut down version of the course schedule in the test resources directory
+        # similar to test above but with a cutoff, so all results before SU27 (non inclusive) should be skipped
+        output: dict[str, list[str]] = extract_sheet_data(load_workbook(self.resource_path / "schedule_input_test.xlsx").active, cutoff="SU27")
+        self.assertListEqual(output["CPSC 1105"], ["SU27", "FA27", "SP28", "SU28", "FA28", "SP29"])
+        self.assertListEqual(output["CPSC 1555"], [])
+        self.assertListEqual(output["CPSC 2105"], ["FA27", "SP28", "FA28", "SP29"])
+        self.assertListEqual(output["CPSC 2108"], ["SU27", "FA27", "SP28", "SU28", "FA28", "SP29"])
+        self.assertListEqual(output["CYBR 3108"], ["SP28", "SP29"])
+        self.assertListEqual(output["CPSC 3137"], ["FA28"])
+        self.assertListEqual(output["CPSC 3165"], ["SU27", "FA27", "SP28", "SU28", "FA28", "SP29"])
+        self.assertEqual(7, len(output.keys()))
