022afe63c5802ef3a8ec8a22f080bd58a2538563
Pedro <Pedro-T@users.noreply.github.com>
2024-10-24 17:57:49 -0400

    amend tests to support change in scraper structure

--- tests/test_course_info_scraper.py
+++ tests/test_course_info_scraper.py
@@ -1,23 +1,26 @@
 import unittest
 
 from bs4 import BeautifulSoup
-from class_planning_tool.input_data.prereq_scraper import get_prerequisites, parse_prereq_block, extract_information
+from class_planning_tool.input_data.prereq_scraper import Scraper
 
 class TestClassScheduleParsing(unittest.TestCase):
 
+    def setUp(self):
+        self.scraper = Scraper()
+
     def test_preqeq_parsing_single(self):
         block: str = "CPSC 1301K with a minimum grade of C"
-        results: list[list[str]] = parse_prereq_block(block)
+        results: list[list[str]] = self.scraper.parse_prereq_block(block)
         self.assertListEqual([["CPSC 1301K"]], results)
     
     def test_prereq_parsing_alternates(self):
         block: str = "CPSC 1301 with a minimum grade of C or CPSC 1301H with a minimum grade of C or CPSC 1301K with a minimum grade of C or CPSC 1301I with a minimum grade of C"
-        results: list[list[str]] = parse_prereq_block(block)
+        results: list[list[str]] = self.scraper.parse_prereq_block(block)
         self.assertListEqual([["CPSC 1301", "CPSC 1301H", "CPSC 1301K", "CPSC 1301I"]], results)
     
     def test_prereq_parsing_multi_group_complex(self):
         block: str = "CPSC 1301 with a minimum grade of C or CPSC 1301H with a minimum grade of C or CPSC 1301K with a minimum grade of C or CPSC 1301I with a minimum grade of C and CPSC 6677 or CPSC 9999 and CPSC 5555R"
-        results: list[list[str]] = parse_prereq_block(block)
+        results: list[list[str]] = self.scraper.parse_prereq_block(block)
         self.assertListEqual([
                 ["CPSC 1301", "CPSC 1301H", "CPSC 1301K", "CPSC 1301I"],
                 ["CPSC 6677", "CPSC 9999"],
@@ -42,13 +45,5 @@ class TestClassScheduleParsing(unittest.TestCase):
 
         with open("tests/resources/course_descriptions_trimmed.html", "r") as f:
             soup: BeautifulSoup = BeautifulSoup(f.read(), "html.parser")
-        results = extract_information(soup)
+        results = self.scraper.extract_information(soup)
         self.assertDictEqual(expected, results)
-
-
-    @unittest.skip("Full end to end test with real query to site, only execute this when necessary.")
-    def test_end_to_end(self):
-        """
-        Due to the very long list on the actual page, this will only be a spot check for three randomly chosen courses.
-        """
-        pass
