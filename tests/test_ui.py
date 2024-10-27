3c27a9a3c2867075d657973737d68a619ddd5aa9
OursCat <jianzhang0412@gmail.com>
2024-10-26 22:29:35 -0500

    add restart button and test file

---
+++ tests/test_ui.py
@@ -0,0 +1,12 @@
+
+
+import unittest
+
+class TestDashboard(unittest.TestCase):
+    def test(self):
+        result = 5
+
+        self.assertEqual(result, 5)
+
+if __name__ == '__main__':
+    unittest.main()
